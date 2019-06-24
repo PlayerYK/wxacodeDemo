f97e4e0ecadc8aca92fec2ed72c01749fbcfd958
yukun <kamal.yu@gmail.com>
2019-06-24 17:55:23 +0800

    云函数代码整理

--- cloudfunctions/getqr/index.js
+++ cloudfunctions/getqr/index.js
@@ -17,54 +17,53 @@ exports.main = async (event, context) => {
   //   temp_id
   // }
 
-  // 先尝试获取，文件存在就直接返回
-  let getURLReault = await cloud.getTempFileURL({
-    fileList: [temp_id]
-  });
-  // return getURLReault;
-  let fileObj = getURLReault.fileList[0];
-
-  if (fileObj.tempFileURL != ''){
-    fileObj.fromCache = true;
-    return fileObj;
-  }else{
-    try {
-      const wxacodeResult = await cloud.openapi.wxacode.getUnlimited({
-        scene: event.scene,
-        page: event.page,
-        width: 240
-      })
-      // return wxacodeResult;
-
-      // console.log(wxacodeResult)
-      if (wxacodeResult.errCode === 0) {
-        // 上传到云存储
-        const uploadResult = await cloud.uploadFile({
-          cloudPath: 'qr/' + qr_name_hash + '.jpg',
-          fileContent: wxacodeResult.buffer,
-        });
-        // return uploadResult;
-
-        if (uploadResult.fileID) {
-          // 获取图片临时路径
-          getURLReault = await cloud.getTempFileURL({
-            fileList: [uploadResult.fileID]
-          });
-          fileObj = getURLReault.fileList[0];
-          fileObj.fromCache = false;
-          // 上传成功，获取文件临时url，返回
-          return fileObj;
-        } else {
-          //上传失败，返回错误信息
-          return uploadResult;
-        }
-      } else {
-        // 生成二维码失败，返回错误信息
-        return wxacodeResult;
-      }
-    } catch (err) {
-      return err
+  try {
+    // 先尝试获取文件，存在就直接返回临时路径
+    let getURLReault = await cloud.getTempFileURL({
+      fileList: [temp_id]
+    });
+    // return getURLReault;
+    let fileObj = getURLReault.fileList[0];
+    if (fileObj.tempFileURL != '') {
+      fileObj.fromCache = true;
+      return fileObj;
     }
+
+    // 生成小程序码
+    const wxacodeResult = await cloud.openapi.wxacode.getUnlimited({
+      scene: event.scene,
+      page: event.page,
+      width: 240
+    })
+    // return wxacodeResult;
+    if (wxacodeResult.errCode != 0) {
+      // 生成二维码失败，返回错误信息
+      return wxacodeResult;
+    }
+
+    // 上传到云存储
+    const uploadResult = await cloud.uploadFile({
+      cloudPath: 'qr/' + qr_name_hash + '.jpg',
+      fileContent: wxacodeResult.buffer,
+    });
+    // return uploadResult;
+    if (!uploadResult.fileID) {
+      //上传失败，返回错误信息
+      return uploadResult;
+    }
+
+    // 获取图片临时路径
+    getURLReault = await cloud.getTempFileURL({
+      fileList: [uploadResult.fileID]
+    });
+    fileObj = getURLReault.fileList[0];
+    fileObj.fromCache = false;
+
+    // 上传成功，获取文件临时url，返回临时路劲的查询结果
+    return fileObj;
+
+  } catch (err) {
+    return err
   }
-  
+
 }
